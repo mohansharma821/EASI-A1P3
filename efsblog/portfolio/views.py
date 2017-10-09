64dd1feac4e9cb8cac68087a2cbd809eb1fa0a22
Mohan Sharma <mohansharma821@gmail.com>
2017-10-08 21:14:30 -0500

    Update views.py

--- efsblog/portfolio/views.py
+++ efsblog/portfolio/views.py
@@ -24,6 +24,25 @@ def customer_list(request):
 
 
 @login_required
+def customer_new(request):
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.created_date = timezone.now()
+            customer.save()
+            customers = Customer.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'portfolio/customer_list.html',
+                          {'customers': customers})
+    else:
+        form = CustomerForm()
+        # print("Else")
+    return render(request, 'portfolio/customer_new.html', {'form': form})
+
+
+
+
+@login_required
 def customer_edit(request, pk):
    customer = get_object_or_404(Customer, pk=pk)
    if request.method == "POST":
